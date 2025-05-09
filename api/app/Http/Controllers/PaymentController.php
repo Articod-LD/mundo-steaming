966f3c2874caaa932d116ad54430f7df70528b8a
Ivan Caviedes <ivancaviedes99@gmail.com>
2025-05-09 14:19:24 -0500

    obteniendo status

--- api/app/Http/Controllers/PaymentController.php
+++ api/app/Http/Controllers/PaymentController.php
@@ -112,10 +112,9 @@ class PaymentController extends Controller
             $client = new PaymentClient();
             $payment = $client->get($paymentIdReference);
             $paymentReference = $payment->external_reference;
+            $paymentStatus = $payment->status;
         }
-        return redirect()->away(
-            trim(env('FRONTEND_URL_SUSCRIPTION')) . '?status=unknown&message=' . $paymentStatus
-        );
+
 
         if (!$paymentReference && !$paymentStatus) {
             return redirect()->away(
